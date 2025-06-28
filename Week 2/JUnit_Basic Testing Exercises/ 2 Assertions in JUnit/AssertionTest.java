e0fe374967bc35093892e702c390c053729eb24c
thullurivamsi <thullurivamsi51@gmail.com>
2025-06-28 21:02:35 +0530

    Create AssertionTest.java

---
+++ Week 2/JUnit_Basic Testing Exercises/ 2 Assertions in JUnit/AssertionTest.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.* ;
+import org.junit.Test;
+
+public class AssertionTest {
+    @Test
+    public void testAssertions() {
+        // Assert equals
+        assertEquals(5, 2 + 3);
+        // Assert true
+        assertTrue(5 > 3);
+        // Assert false
+        assertFalse(5 < 3);
+        // Assert null
+        assertNull(null);
+        // Assert not null
+        assertNotNull(new Object());
+    }
+
+}
