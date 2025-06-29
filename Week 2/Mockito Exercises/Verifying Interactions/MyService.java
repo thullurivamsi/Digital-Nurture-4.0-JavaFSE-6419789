112ff92286d5f925346f00c10f98dd7dd9905d87
thullurivamsi <thullurivamsi51@gmail.com>
2025-06-29 11:14:27 +0530

    Add files via upload

---
+++ Week 2/Mockito Exercises/Verifying Interactions/MyService.java
@@ -0,0 +1,12 @@
+public class MyService {
+
+     ExternalApi api ;
+
+     public MyService(ExternalApi api) {
+         this.api = api ;
+     }
+
+     public String fetchData() {
+         return api.getData() ;
+     }
+}
