d00487bbf89e48a13e848e5de3ebc31d93afa82b
thullurivamsi <thullurivamsi51@gmail.com>
2025-07-01 21:22:53 +0530

    Update BookService.java

--- Week 3/Spring Core_Maven/1. LibraryManagement/BookService.java
+++ Week 3/Spring Core_Maven/1. LibraryManagement/BookService.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 public class BookService {
 
-    private final BookRepository bookRepository ;
+    private BookRepository bookRepository ;
 
     public BookService(BookRepository bookRepository) {
         this.bookRepository = bookRepository ;
