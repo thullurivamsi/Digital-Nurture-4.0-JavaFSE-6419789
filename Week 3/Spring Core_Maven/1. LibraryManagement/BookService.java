c2f3614f2990f3c2aa9e8533498492c3c08c182a
thullurivamsi <thullurivamsi51@gmail.com>
2025-07-01 21:01:12 +0530

    Create BookService.java

---
+++ Week 3/Spring Core_Maven/1. LibraryManagement/BookService.java
@@ -0,0 +1,22 @@
+package com.library.Service;
+
+import com.library.Repository.BookRepository;
+
+import java.util.ArrayList;
+
+public class BookService {
+
+    private final BookRepository bookRepository ;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository ;
+    }
+
+    public void showAllBooks() {
+        ArrayList<String> books = bookRepository.getRepository() ;
+        System.out.println("Books available for service are : ");
+        for(String bookName : books )
+            System.out.println(bookName);
+    }
+
+}
