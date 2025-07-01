6cf0d4b34dd3503fe1283bd1a41ebc81deb60d49
thullurivamsi <thullurivamsi51@gmail.com>
2025-07-01 21:32:51 +0530

    Create BookService.java

---
+++ Week 3/Spring Core_Maven/2. Dependency Injection/BookService.java
@@ -0,0 +1,23 @@
+
+package com.library.Service;
+
+import com.library.Repository.BookRepository;
+
+import java.util.ArrayList;
+
+public class BookService {
+
+    private BookRepository bookRepository ;
+
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository ;
+    }
+
+    public void showAllBooks() {
+        ArrayList<String> books = bookRepository.getRepository() ;
+        System.out.println("Books available for service are : ");
+        for(String bookName : books )
+            System.out.println(bookName);
+    }
+
+}
