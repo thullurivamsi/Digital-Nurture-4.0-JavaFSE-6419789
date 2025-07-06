b54f207daa84ef5e115317efae81e5f6a5f7f06e
thullurivamsi <thullurivamsi51@gmail.com>
2025-07-06 11:35:00 +0530

    Create CountryService.java

---
+++ Week 3/1. spring-data-jpa-handson/Spring Data JPA - Quick Example/main/java/service/CountryService.java
@@ -0,0 +1,44 @@
+package com.cognizant.orm_learn.service;
+
+import java.util.List;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.cognizant.orm_learn.model.Country;
+import com.cognizant.orm_learn.repository.CountryRepository;
+
+@Service
+public class CountryService {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    @Transactional
+    public List<Country> getAllCountries() {
+        return countryRepository.findAll();
+    }
+
+    @Transactional
+    public Country findCountryByCode(String countryCode) {
+        return countryRepository.findById(countryCode).orElse(null);
+    }
+
+    @Transactional
+    public void addCountry(Country country) {
+        countryRepository.save(country);
+    }
+
+    @Transactional
+    public void updateCountry(String code, String newName) {
+        Country country = countryRepository.findById(code).orElse(null);
+        if (country != null) {
+            country.setName(newName);
+            countryRepository.save(country);
+        }
+    }
+
+    @Transactional
+    public void deleteCountry(String code) {
+        countryRepository.deleteById(code);
+    }
+}
