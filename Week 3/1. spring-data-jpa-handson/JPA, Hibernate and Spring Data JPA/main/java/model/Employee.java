20aded69532df69c39685da36e5b0d28890f13e0
thullurivamsi <thullurivamsi51@gmail.com>
2025-07-06 15:00:15 +0530

    Create Employee.java

---
+++ Week 3/1. spring-data-jpa-handson/JPA, Hibernate and Spring Data JPA/main/java/model/Employee.java
@@ -0,0 +1,22 @@
+package com.ormlearn.OrmLearn.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private Double salary;
+    private String department;
+}
