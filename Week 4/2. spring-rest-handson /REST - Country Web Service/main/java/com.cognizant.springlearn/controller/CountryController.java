f73beaff1e634d06939df5fd1d87a15049aade66
thullurivamsi <thullurivamsi51@gmail.com>
2025-07-13 21:21:46 +0530

    Create CountryController.java

---
+++ Week 4/2. spring-rest-handson /REST - Country Web Service/main/java/com.cognizant.springlearn/controller/CountryController.java
@@ -0,0 +1,26 @@
+package com.cognizant.springlearn.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognizant.springlearn.model.Country;
+
+@RestController
+@RequestMapping("/country")
+public class CountryController {
+
+    @GetMapping
+    public Country getCountryIndia() {
+        // Load the application context
+        ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+        
+        // Get the India bean
+        Country country = (Country) context.getBean("in");
+        
+        return country;
+    }
+}
