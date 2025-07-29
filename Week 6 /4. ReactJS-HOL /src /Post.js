92b10d94d67b0b623a9ff6f545aa51cdb858e492
thullurivamsi <thullurivamsi51@gmail.com>
2025-07-29 21:34:41 +0530

    Create Post.js

---
+++ Week 6 /4. ReactJS-HOL /src /Post.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Post extends React.Component {
+  render() {
+    const { title, body } = this.props;
+    return (
+      <div style={{ marginBottom: '20px', borderBottom: '1px solid #ccc' }}>
+        <h2>{title}</h2>
+        <p>{body}</p>
+      </div>
+    );
+  }
+}
+
+export default Post;
