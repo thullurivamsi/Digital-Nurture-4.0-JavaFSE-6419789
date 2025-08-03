a953e4e586cac2547bcf28178e2cfd95b7ef51b6
thullurivamsi <thullurivamsi51@gmail.com>
2025-08-03 17:22:22 +0530

    Create App.js

---
+++ Week 7 /10. ReactJS-HOL /src /App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import OfficeSpaceRental from './Components/OfficeSpaceRental';
+
+function App() {
+  return (
+    <div className="App">
+      <OfficeSpaceRental />
+    </div>
+  );
+}
+
+export default App;
